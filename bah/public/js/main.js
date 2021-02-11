b6b6e401e0f2e04e275e20e39e19beb26d1cb031
Віталій Віталсо <vitalii@Vitalijs-MacBook-Pro.local>
2021-02-11 15:48:17 +0200

    new project - Bah

---
+++ bah/public/js/main.js
@@ -0,0 +1,109 @@
+$(document).ready(function() {
+
+  // Slick slider - Gallery pack
+  $('.gallery-pack ul').slick({
+    variableWidth: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 10000,
+    pauseOnHover: false,
+    cssEase: 'linear',
+    useTransform: false,
+    arrows: false
+  });
+
+  // Scroll Reveal - effects when scroll page
+  ScrollReveal().reveal('.animated' , { delay: 500 , distance: '100px' });
+
+  // Scroll configure section
+  /*$('.side-scrolling.left ul').slick({
+    autoplay: true,
+    infinite: true,
+    autoplaySpeed: 5000,
+    centerMode: true,
+    centerPadding: '0',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    focusOnSelect: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    cssEase: 'linear',
+    useTransform: false,
+    arrows: false,
+    vertical: true
+  });
+
+  $('.side-scrolling.right ul').slick({
+    autoplay: true,
+    infinite: true,
+    autoplaySpeed: 5000,
+    centerMode: true,
+    centerPadding: '0',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    focusOnSelect: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    cssEase: 'linear',
+    useTransform: false,
+    arrows: false,
+    vertical: true
+    //verticalReverse: true
+  });*/
+
+  // Toogle class button menu
+  $('.toggle-nav').on('click' , function(){
+    $(this).toggleClass('open');
+    $('.nav').toggleClass('open');
+  });
+
+  // Popup
+  $('.whatsapp-header , .index-link , .via-phone , .phone-popup').magnificPopup({
+    type: 'inline',
+    preloader: false,
+    modal: false
+  });
+
+  // FAQ
+  $('.faq-title').on('click' , function() {
+    $(this).closest('.faq-item').toggleClass('open');
+  });
+
+  // Play video
+  $('.play-video').on('click', function(e) {
+
+    $("#video")[0].src += "&autoplay=1";
+    $(this).hide();
+    $('#video').show();
+    e.preventDefault();
+
+  });
+
+  // Scrool
+  $(window).on('scroll' , function() {
+
+    /*if ($(window).scrollTop() > 90) {
+      $('.solid-btn').addClass('active');
+    } else {
+      $('.solid-btn').removeClass('active');
+    }*/
+
+    // Change background when scroll to section #Preveliges
+    if ($(window).scrollTop() >= $('#preveliges').offset().top) {
+      $('#preveliges').addClass('bg-change');
+    }
+
+    var currentScroll = Number(Math.trunc($(window).scrollTop() / 200));
+
+    $('.square.first-orange').css({ transform: 'rotate(-' + (currentScroll - 15) + 'deg)' });
+    $('.square.blue').css({ transform: 'rotate(-' + (currentScroll - 10 ) + 'deg)' });
+    $('.square.grey-color').css({ transform: 'rotate(' + (currentScroll - 5 ) + 'deg)' });
+    $('.square.second-green').css({ transform: 'rotate(-' + (currentScroll - 13 ) + 'deg)' });
+    $('.square.light-green').css({ transform: 'rotate(' + (currentScroll - 20 ) + 'deg)' });
+    $('.square.last-orange').css({ transform: 'rotate(-' + (currentScroll - 15 ) + 'deg)' });
+
+  });
+
+})
