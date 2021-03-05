fa512edd8dd45133e5f1f1fa16967c3c95667e91
Віталій Віталсо <vitalii@Vitalijs-MacBook-Pro.local>
2021-03-05 20:11:41 +0200

    some changes

--- landing/js/main.js
+++ landing/js/main.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
   // Scroll to section
   $('.nav a').on('click' , function(e){
     e.preventDefault();
+    $(this).closest('.nav').removeClass('open');
     var href = $(this).attr('href');
     $('html, body').animate({
         scrollTop: $(href).offset().top
