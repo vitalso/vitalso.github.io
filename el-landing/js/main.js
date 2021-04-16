bc6981da3eb5d6b2d993f36f2f58b23567c0a107
Віталій Віталсо <vitalii@Vitalijs-MacBook-Pro.local>
2021-04-16 12:49:48 +0300

    new project

---
+++ el-landing/js/main.js
@@ -0,0 +1,66 @@
+$(function () {
+
+  // Responsive nav
+  $('.toggle-nav').on('click' , function() {
+    $(this).toggleClass('open');
+  });
+
+  // Expert slider
+  $('.experts-slider').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    arrows: false,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  // Example of works slider
+  $('.example-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  // FAQ
+  $('.faq-item').on('click' , function() {
+    $(this).toggleClass('open');
+  });
+
+  // Popup with video
+  $('.popup-youtube').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: true
+  });
+
+});
