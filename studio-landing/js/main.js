f5809471a7a9cd218511b18fe7afc143e61525d8
Віталій Віталсо <vitalii@Vitalijs-MacBook-Pro.local>
2021-05-14 11:35:37 +0300

    new project

---
+++ studio-landing/js/main.js
@@ -0,0 +1,50 @@
+$(function () {
+
+  // Responsive nav
+  $('.toggle-nav').on('click' , function() {
+    $(this).toggleClass('open');
+  });
+
+  // Stream toggle cover
+  $('.stream-list a').on('click' , function(e){
+    e.preventDefault();
+    var index = $(this).closest('li').index();
+    $('.stream-list a').removeClass('current-item');
+    $(this).addClass('current-item');
+    $('.stream-cover').hide();
+    $('.stream-cover').eq(index).show();
+  });
+
+  // Example of works slider
+  $('.clients-slider').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+});
