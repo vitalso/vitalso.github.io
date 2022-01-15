b7dff4956dde2a993cbb9f4df6afdea6831601bd
vitalii <lgphone.vs@gmail.com>
2022-01-15 12:50:36 +0200

    changes

--- keyword/js/custom-keyword.js
+++ keyword/js/custom-keyword.js
@@ -20,11 +20,22 @@ $(document).ready(function(){
     increaseArea: '20%' // optional
   });
 
+  // Structure switch
   $('.structure-list li').on('click' , function(){
     $('.structure-list li').removeClass('active-structure');
     $('.structure-list li').find('.iradio_square-green').removeClass('checked');
     $(this).addClass('active-structure');
     $(this).find('.iradio_square-green').addClass('checked');
+  });
+
+  // Select Step 1 choose the campaign
+  $('#choose-campaign').on('change' , function(){
+    var $option = $(this).find('option:selected');
+    var value = $option.val();
+
+    $('.choosen-radio-group ul').addClass('d-none');
+    $('.choosen-radio-group').find('#' + value).removeClass('d-none');
+    console.log(value);
   });
 
 });
