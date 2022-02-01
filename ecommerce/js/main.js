117e201f5c013d5787d1bec8a5df43d0cf7b7071
vitalii <lgphone.vs@gmail.com>
2022-02-01 17:09:38 +0200

    changes

---
+++ ecommerce/js/main.js
@@ -0,0 +1,26 @@
+$(function () {
+
+  // Mobile toggle menu
+  $('.toggle-nav').on('click' , function(){
+    $(this).toggleClass('open');
+    $('header nav').toggleClass('open');
+    $('body').toggleClass('overflow-hidden');
+  });
+
+  // Smooth scroll
+  $('header nav a').on('click' , function(){
+    $('header nav , .toggle-nav').removeClass('open');
+    $('body').removeClass('overflow-hidden');
+    var target = $(this).attr('href');
+    $('html, body').animate({scrollTop: $(target).offset().top}, 500);
+  });
+
+  // Slick slider
+  $('.index-slider').slick({
+    infinite: true,
+    arrows: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+
+});
