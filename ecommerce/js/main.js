56e8872c6238f41be7aa0fce03b034cfd1bb0b0c
vitalii <lgphone.vs@gmail.com>
2022-02-21 22:48:00 +0200

    changes

--- ecommerce/js/main.js
+++ ecommerce/js/main.js
@@ -15,6 +15,49 @@ $(function () {
     $('html, body').animate({scrollTop: $(target).offset().top}, 500);
   });*/
 
+  // Validate form
+  $('#login-form').validate({
+    errorClass: "error error-label",
+    messages : {
+      login: {
+        required: "Введено невірну адресу ел. пошти або номер телефону"
+      },
+
+      password: {
+        required: "Поле обов`язкове для заповнення"
+      }
+    }
+  });
+
+  $('#signup-form').validate({
+    errorClass: "error error-label",
+    messages : {
+      name: {
+        required: "Введіть своє ім'я кирилицею"
+      },
+
+      surname: {
+        required: "Введіть своє прізвище кирилицею"
+      },
+
+      phone: {
+        required: "Введіть номер мобільного телефону"
+      },
+
+      phone: {
+        required: "Введіть номер мобільного телефону"
+      },
+
+      email: {
+        required: "Введіть свою ел. пошту"
+      },
+
+      password: {
+        required: "Поле обов`язкове для заповнення"
+      }
+    }
+  });
+
   // Slick slider
   $('.index-slider').slick({
     infinite: true,
@@ -30,6 +73,26 @@ $(function () {
         }
       }
     ]
+  });
+
+  // Popup
+  $('.open-popup').magnificPopup({
+    type:'inline',
+    showCloseBtn: false
+  });
+
+  // Toogle input password
+  $('.form-show-password').on('click' , function(e){
+    e.preventDefault();
+
+    let passwordInput = $(this).closest('div').find('input');
+
+    if ( passwordInput.attr('type') == 'password' ) {
+      passwordInput.prop('type' , 'text');
+    } else {
+      passwordInput.prop('type' , 'password');
+    }
+
   });
 
 });
