f87e5dd31ba4e461d4c5d7c17f121d8227ff6591
Vitaliy <lgphone.vs@gmail.com>
2022-08-05 15:40:03 +0300

    changes

--- rocket/js/custom.js
+++ rocket/js/custom.js
@@ -3,6 +3,12 @@ $(function () {
     // Select plan in modal
     $('.plan-switcher input[type="radio"]').on('click' , function(){
         $('#modal-price .current-price').text($(this).val());
+
+        if ( $(this).is(':checked') && $(this).attr('id') == 'pay-monthly' ) {
+            $('#go-to-payment').attr('href' , 'test.php?sub=monthly');
+        } else {
+            $('#go-to-payment').attr('href' , 'test.php?sub=annually');
+        }
     });
 
     // Select plan on switch button
@@ -13,12 +19,10 @@ $(function () {
     $('#switch').on('change' , function(){
         if ( $(this).is(':checked') ) {
             $('.price-switcher .current-price').text(annuallyPrice);
-            $('#change-plan').addClass('disabled').attr('href' , 'test.php?sub=annually');;
-            $('.plan-switch').closest('.ibox-content').find('.alert').addClass('d-none');
+            $('#change-plan').addClass('disabled').attr('href' , 'test.php?sub=annually');
         } else {
             $('.price-switcher .current-price').text(monthlyPrice);
             $('#change-plan').removeClass('disabled').attr('href' , 'test.php?sub=monthly');
-            $('.plan-switch').closest('.ibox-content').find('.alert').removeClass('d-none');
         }
     });
 
