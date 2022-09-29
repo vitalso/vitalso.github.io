2ef7d9a01e542208925002c2d684b28d1950e1aa
Vitaliy <lgphone.vs@gmail.com>
2022-09-29 12:00:40 +0300

    new project

---
+++ sw-koln/js/main.js
@@ -0,0 +1,43 @@
+$(function () {
+
+    // Mobile menu button toggle
+    $('.navbar-toggler').on('click' , function(){
+      $(this).toggleClass('open');
+    });
+
+    // Hero section slider
+    $('.hero-slider').slick({
+        infinite: true,
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+
+    // News slider
+    $('.news-slider').slick({
+        infinite: true,
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 2,
+                  vertical: true,
+                  dots: false,
+                  swipe: false
+                }
+              }
+          ]
+    });
+
+});
