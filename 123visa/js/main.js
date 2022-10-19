46f0dcacaae710006db31cc9da100a62a000578f
Vitaliy <lgphone.vs@gmail.com>
2022-10-19 23:33:27 +0300

    changes

--- 123visa/js/main.js
+++ 123visa/js/main.js
@@ -10,7 +10,9 @@ $(function () {
       e.preventDefault();
 
       let currentOption = $(this).text();
-      $(this).closest('.dropdown-select').find('button[data-bs-toggle]').text(currentOption);
+      let currentFlag = $(this).attr('href');
+      $(this).closest('.dropdown-select').find('button[data-bs-toggle] span').text(currentOption);
+      $(this).closest('.dropdown-select').find('button[data-bs-toggle] img').attr('src' , currentFlag);
 
     });
 
@@ -52,6 +54,70 @@ $(function () {
         inputPasword.attr('type' , 'password');
       }
     });
+
+    // Init jQuery steps
+    var steps = $('.step-app').steps({
+      onFinish: function () { 
+        //alert('complete');
+        $('#app-form').addClass('d-none'); // Hide section with steps
+        $('#review-application').removeClass('d-none'); // Show order summary for application
+      }
+    });
+
+    steps_api = steps.data('plugin_Steps');
+
+    $('.step-footer button').on('click', function () {
+      var idx = steps_api.getStepIndex()+1;
+
+      // Show sidebar question
+      if ( idx == 3 ) {
+        $('.question-sidebar').removeClass('d-none');
+      }
+
+      // Show previous button
+      if ( idx == 4 ) {
+        $('.step-footer button[data-step-action="prev"]').removeClass('d-none');
+      }
+
+		});
+
+    // Edit Traveler
+    $('.edit-traveler').on('click', function (e) {
+      e.preventDefault();
+      steps_api.setStepIndex(2);
+		});
+
+    // Datepicker
+    $('.datepicker').datepicker({
+      format: 'dd MM yyyy',
+      autoclose: true
+    });
+
+    // File upload
+    $('.files input[type="file"]').change(function() {
+      var file = $(this)[0].files[0].name;
+      $('.file-upload-list .item-file').removeClass('d-none').find('.file-name').text(file);
+      $('.file-upload-list .item-file .progress-bar').css('width' , '100%');
+    });
+
+    // Phone number
+    /*var input = document.querySelector("#phone");
+      window.intlTelInput(input, {
+        autoHideDialCode: false,
+        nationalMode: false,
+        utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/8.4.6/js/utils.js"
+    });*/
+
+    // Question sidebar
+    $('.question-sidebar .close-sidebar').on('click' , function(){
+      $(this).closest('.question-sidebar').addClass('d-none');
+    });
+
+    // Attention button
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
+    })
 
     // Button collapse change the class
     $('.btn-collapse').on('click' , function(){
