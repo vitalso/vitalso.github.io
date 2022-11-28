c2b6da802baa3e68be6ad46f24141650d3200468
Vitaliy <lgphone.vs@gmail.com>
2022-11-28 14:52:16 +0200

    new project

---
+++ demo-chat/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    minHeight: {
+      '1/2': '50vh',
+    }
+  },
+  plugins: [],
+}
