650ecafa600cb6d1ea7f35243f7e28ce53a74a09
Vitaliy <lgphone.vs@gmail.com>
2022-12-04 18:25:41 +0200

    changes

--- demo-chat/public/js/main.js
+++ demo-chat/public/js/main.js
@@ -1,12 +1,10 @@
 $(function () {
 
-    var url = 'https://stepsforward.com.au/buddylink/2/control/interactions.php?action=getinfo&deviceCode=4RXPDLJPDI&event=Launch&requestType=LaunchRequest&turn=0'
+    var url = 'https://stepsforward.com.au/buddylink/2/control/interactions.php?action=getinfo&web=Y&deviceCode=4RXPDLJPDI&event=Launch&requestType=LaunchRequest&turn=0'
+    var screenText = $('#screenText');
+    var serverSay = $('#serverSay');
+    //var resp = [];
     $('#open-chat').on('click' , function(){
-        /*$.getJSON("https://stepsforward.com.au/buddylink/2/control/interactions.php?action=getinfo&deviceCode=4RXPDLJPDI&event=Launch&requestType=LaunchRequest&turn=0", function(result){
-            //alert("Data: " + data + "\nStatus: " + status);
-
-            console.log(result);
-        });*/
         $.ajax({
             url: url,
             headers: {
@@ -18,6 +16,15 @@ $(function () {
             },
             success: function (result) {
                 console.log(result);
+                console.log(result.screenText);
+                screenText.text(result.screenText);
+                serverSay.text(result.serverSays);
+                var resp = result.expect_resp_arr;
+                console.log(resp);
+                for (i=0; i < resp.length; i++){
+                    console.log(i, resp[i]);
+                    $('.chat-answer').append("<div class='mb-3'><input type='radio' name='radio' id="+i+"><label for="+i+">"+resp[i]+"</label></div>");
+                }
             },
             error: function () {
                 console.log("error");
