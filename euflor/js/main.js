d987a85eb705c9725db13d6fd67ac93455a38161
Vitaliy <lgphone.vs@gmail.com>
2023-02-06 11:00:00 +0200

    changes

--- euflor/js/main.js
+++ euflor/js/main.js
@@ -1,15 +1,54 @@
 $(function () {
 
-  // Fixed header
-  var header = $('header');
-  var headerHeight = header.outerHeight();
+  // Circle animation/slider
+  const slider = $('.circle-slider');
+  const section = $('#circle-section');
+  let blocked = false;
+  let blockTimeout = null;
+  let prevDeltaY = 0;
 
-  $('.navbar-toggler').on('click' , function() {
-    //header.removeClass('fixed');
-    if ( header.is('.fixed') ) {
-      $('.logo').toggleClass('open-menu');
-    }
-    //$('html').toggleClass('overflow-hidden');
+  slider.slick({
+    infinite: false,
+    arrows: false,
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
   });
+
+  section.on('mousewheel DOMMouseScroll wheel' , function(e){
+    let deltaY = e.originalEvent.deltaY;
+    //e.preventDefault();
+    //e.stopPropagation();
+
+    clearTimeout(blockTimeout);
+    blockTimeout = setTimeout(function(){
+      blocked = false;
+    }, 50);
+
+    
+    if (deltaY > 0 && deltaY > prevDeltaY || deltaY < 0 && deltaY < prevDeltaY || !blocked) {
+      blocked = true;
+      prevDeltaY = deltaY;
+
+      e.preventDefault();
+      e.stopPropagation();
+
+      if (deltaY > 0) {
+        slider.slick('slickNext');
+        $('.wrap-circle-item').removeClass (function (index, className) {
+          return (className.match (/\bslide-\S+/g) || []).join(' ');
+        });
+        $('.wrap-circle-item').addClass('slide-' + slider.find('.slick-current.slick-active').attr('data-slick-index'));
+      } else {
+        slider.slick('slickPrev');
+        $('.wrap-circle-item').removeClass (function (index, className) {
+          return (className.match (/\bslide-\S+/g) || []).join(' ');
+        });
+        $('.wrap-circle-item').addClass('slide-' + slider.find('.slick-current.slick-active').attr('data-slick-index'));
+      }
+
+    }
+
+  })
 
 });
