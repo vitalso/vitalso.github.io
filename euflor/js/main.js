79975519d2b3bb51af32c297a9cb655800d67a37
Vitaliy <lgphone.vs@gmail.com>
2023-02-16 14:43:41 +0200

    changes

--- euflor/js/main.js
+++ euflor/js/main.js
@@ -74,9 +74,73 @@ $(function () {
     arrows: true,
     dots: false,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
-    variableWidth: true
+    variableWidth: true,
+    adaptiveHeight: true
+  });
+
+  // Card small slider
+  $('.card-small-slider').slick({
+    infinite: true,
+    arrows: false,
+    dots: false,
+    autoplayspeed: 1500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+
+  $('.right-slider').slick({
+    infinite: true,
+    arrows: false,
+    dots: false,
+    autoplayspeed: 1500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    rtl: true
+  });
+
+  // Calculate function
+  var numberResult = $('.calc-result');
+  var bagsItem = $('.bags-item');
+  var showBags = $('.show-bags');
+  var bags = numberResult.find('#bags');
+  var liter = numberResult.find('#liter');
+
+  $('.calc-form').on('submit' , function(e){
+    e.preventDefault();
+
+    var form = $(this);
+    var height = form.find('input[name="height"]').val();
+    var width = form.find('input[name="width"]').val();
+    var long = form.find('input[name="long"]').val();
+
+    if ( height.length !== 0 || width.length !== 0 || long.length !== 0 ) {
+      var square = height*width*long;
+      var bagsResult = parseInt(square*5.33+1); // 5.33 it's count of bugs for one square meter
+      var literResult = parseFloat(square*0.32); // 0.32 it's count of liters for one square meter
+
+      bags.text(bagsResult);
+      liter.text(literResult);
+      showBags.text(bagsResult);
+      numberResult.removeClass('d-none');
+      bagsItem.removeClass('d-none');
+
+      console.log(height , width , long , square);
+    } else {
+      console.log('error');
+    }
+
+    //console.log('okay');
+
   });
 
   // AOS scroll animation
