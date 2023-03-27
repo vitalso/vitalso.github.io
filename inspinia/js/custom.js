12d16e556a4ac3f4a81d839a21a83796583809d6
Vitaliy <lgphone.vs@gmail.com>
2023-03-27 17:06:40 +0300

    new project

---
+++ inspinia/js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+    // Show modal after 30 sec
+    setTimeout(function() {
+        $('#fullScreenModal').modal('show');
+    }, 5000); // change 5000 to 30000 (30000 = 30sec in real time)
+
+})
