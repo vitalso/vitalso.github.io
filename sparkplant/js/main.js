ce8d514187f6dfbf294404dc424865f6c430de14
Vitaliy <lgphone.vs@gmail.com>
2023-04-10 17:57:32 +0300

    changes

--- sparkplant/js/main.js
+++ sparkplant/js/main.js
@@ -1,5 +1,60 @@
 $(function () {
 
-  
+    // AOS scroll animation
+    AOS.init({
+        once: true
+    });
+
+    // Team slider
+    $('.team').slick({
+        infinite: true,
+        arrows: false,
+        dots: false,
+        variableWidth: true,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        touchThreshold: 9,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1
+            }
+          }
+        ]
+    });
+
+    // SVG animation
+    // Line animation
+    if ( $('#line').length != 0 ) {
+        const myLine = new Vivus('line', {duration: 300 , start: 'manual'});
+
+        const offsetScreen = 400;
+        const offset = $('.we-create').offset().top - offsetScreen;
+        $(window).on('scroll' , function(){
+
+            if ($(window).scrollTop() > offset) {
+                myLine.play(0);
+            }
+
+        });
+    }
+
+    // Process animation
+    if ( $('#lineProcess').length != 0 ) {
+        const myProcess = new Vivus('lineProcess', {duration: 300 , start: 'manual'});
+
+        //const offsetScreen = 400;
+        const offsetProcess = $('.our-approach').offset().top;
+        $(window).on('scroll' , function(){
+
+            if ($(window).scrollTop() > offsetProcess) {
+                myProcess.play(0);
+            }
+
+        });
+    }
 
 });
