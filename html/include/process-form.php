29f786f5f7d8d1474a217056d8638d01648aa487
Vitaliy <lgphone.vs@gmail.com>
2023-05-03 15:42:09 +0300

    changes

--- html/include/process-form.php
+++ html/include/process-form.php
@@ -1,30 +1,9 @@
 <?php
-ob_start();
-error_log();
-if(isset($_POST['name']) && isset($_POST['email'])){
-
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$industry = $_POST['industry'];
-$message = $_POST['message'];
-
-$mailHeading = 'MyFreeOnlineTools Contact Form';
-$headers = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers .= 'From: MyFreeOnlineTools <mailmyfreeonlinetools.com>' . PHP_EOL .
-'Reply-To: MyFreeOnlineTools <mailmyfreeonlinetools.com>' . PHP_EOL .
-'X-Mailer: PHP/' . phpversion();
-
-$to = 'lgphone.vs@gmail.com';
-
-
-$messageSent = '<div>Name Sender : '.$name .'</div><div> Name Email : '.$email.' </div><div>Mail Subject : '.$subject.'</div><div> Message : <br/> '.$message.'</div>';
-
-if( $name != '' || $message != '' || $email != ''){
-mail($to,$subject,$messageSent,$headers);
-echo 'Mail Send I Will Get Back to You....';
-}
-}
-
+  $toEmail = "lgphone.vs@gmail.com";
+  $mailHeaders = "From: " . $_POST["name"] . "<". $_POST["email"] .">\r\n";
+  if(mail($toEmail, $_POST["industry"], $_POST["message"], $mailHeaders)) {
+      print "<p class='success'>Mail Sent.</p>";
+  } else {
+      print "<p class='Error'>Problem in Sending Mail.</p>";
+  }
 ?>
