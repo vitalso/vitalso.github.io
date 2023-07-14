4558d55de6142a6f09b5960995e16c358905af7f
Vitaliy <lgphone.vs@gmail.com>
2023-07-14 15:00:00 +0300

    changes

---
+++ ai-feed/tailwind.config.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['"Inter"', 'sans-serif']
+            },
+            colors: {
+                'default-color': '#1D2939',
+                'default-background': '#F9FAFB',
+                'blue-50': '#EFF4FF',
+                'blue-200': '#B2CCFF',
+                'blue-400': '#528BFF',
+                'blue-500': '#2970FF',
+                'gray-100': '#F2F4F7',
+                'gray-200': '#EAECF0',
+                'gray-400': '#98A2B3',
+                'gray-500': '#667085',
+                'gray-600': '#475467',
+                'gray-700': '#344054',
+                'gray-900': '#101828',
+                'success-400': '#32D583',
+            },
+        }
+    }
+}
