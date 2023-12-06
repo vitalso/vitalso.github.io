4d8fcef77460b1d530e06de4e3b13d3a414b4c52
Vitaliy <lgphone.vs@gmail.com>
2023-12-06 17:34:27 +0200

    changes

--- advance/_nuxt/nuxt-icon.37e73957.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./nuxt-icon.vue.0ad2ea64.js";import"./entry.c1605d69.js";export{m as default};
---
+++ advance/_nuxt/nuxt-icon.bac50b69.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./nuxt-icon.vue.40d11592.js";import"./entry.894d92e6.js";export{m as default};
