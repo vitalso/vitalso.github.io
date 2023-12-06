9a3e6c77cea2b1740d3b64b9b877aa794e337028
Vitaliy <lgphone.vs@gmail.com>
2023-12-06 13:23:10 +0200

    new project

---
+++ advance/_nuxt/icon-minus.c0644af2.js
@@ -0,0 +1,6 @@
+const t=`<svg width="16" height="3" viewBox="0 0 16 3" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="Group 1000002279">
+<path id="Vector" d="M15.3398 1.66992H-0.000156403" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
+</g>
+</svg>
+`;export{t as default};
---
+++ advance/_nuxt/nuxt-icon.9b027221.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./nuxt-icon.vue.d0629977.js";import"./entry.c555098b.js";export{m as default};
