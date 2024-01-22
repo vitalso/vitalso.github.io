2f9c28f58b44684a91fc960ed700b8685910aa2b
Vitaliy <lgphone.vs@gmail.com>
2024-01-22 20:16:36 +0200

    changes

--- hempost/js/main.js
+++ hempost/js/main.js
@@ -5,20 +5,27 @@ $(function () {
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
     // Show tracking in header on scroll
-    var position = $('.hero-section').offset().top;
+    var element = $('.hero-section');
     var headerTracking = $('.header-tracking');
+
+    if ( element.length && headerTracking.length ) {
+        var position = element.offset().top;
+
+        $(window).on('scroll' , function(){
+
+            if ( $(window).scrollTop() > position ) {
+                headerTracking.removeClass('d-none');
+                $('header').addClass('small');
+            } else {
+                headerTracking.addClass('d-none');
+                $('header').removeClass('small');
+            }
     
-    $(window).on('scroll' , function(){
+        });
 
-        if ( $(window).scrollTop() > position ) {
-            headerTracking.removeClass('d-none');
-            $('header').addClass('small');
-        } else {
-            headerTracking.addClass('d-none');
-            $('header').removeClass('small');
-        }
-
-    });
+    }
+    
+    
 
     // Show input on mobile
     headerTracking.find('button').on('click' , function(){
