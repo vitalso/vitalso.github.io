e5c03a8675375e876680361bccf9c7afd85f34d6
OSX <osx@OSXs-Mac-mini.local>
2024-04-15 17:36:32 +0300

    changes

--- blitzerkanzlei/js/main.js
+++ blitzerkanzlei/js/main.js
@@ -1,2 +1,15 @@
+// Fixed header on scroll
+const siteHeader = document.getElementById('header');
+const siteHeaderHeight = siteHeader.offsetHeight;
+
+window.addEventListener('scroll', () => {
+    const top = document.documentElement.scrollTop || document.body.scrollTop;
+    if(top > siteHeaderHeight) {
+        siteHeader.classList.add('fixed');
+    } else {
+        siteHeader.classList.remove('fixed');
+    }
+}, false);
+
 // AOS animation
 AOS.init();
