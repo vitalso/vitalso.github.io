4454308554f0e6065cdcee977d4c48624beeb1da
Vitalii <lgphone.vs@gmail.com>
2024-10-28 19:31:04 +0200

    changes

--- adagio-help/js/main.js
+++ adagio-help/js/main.js
@@ -1,3 +1,4 @@
+// Sidebar dropdown
 const dropdown = document.getElementsByClassName('dropdown');
 
 for (i=0; i<dropdown.length; i++) {
@@ -7,3 +8,48 @@ for (i=0; i<dropdown.length; i++) {
     this.classList.toggle('activeDropdown');
   })
 }
+
+// Sidebar on mobile
+const sidebar = document.getElementById('sidebar');
+const sidebarBtn = document.getElementById('toggleSidebarBtn');
+
+// Toggle sidebar
+const toggleSidebar = function() {
+  sidebar.classList.toggle('-left-[248px]');
+}
+
+// Open/close sidebar when click at button
+sidebarBtn.addEventListener('click', (e) => {
+  e.stopPropagation();
+  toggleSidebar();
+});
+
+// Close sidebar when click at link
+document.documentElement.addEventListener('click' , function(){
+  if ( !sidebar.classList.contains('-left-[248px]') ) {
+    toggleSidebar();
+  }
+});
+
+// Table content
+const tableContent = document.getElementById('tableContent');
+const tableContentBtn = document.getElementById('tableContentBtn');
+const tableContentBtnIcon = document.querySelector('#tableContentBtn svg');
+
+const toggleTableContent = function() {
+  tableContent.classList.toggle('hidden');
+  tableContentBtnIcon.classList.toggle('rotate-90');
+}
+
+// Toggle table content
+tableContentBtn.addEventListener('click' , function(e){
+  e.stopPropagation();
+  toggleTableContent();
+  sidebar.classList.add('-left-[248px]');
+});
+
+document.documentElement.addEventListener('click' , function(){
+  if ( !tableContent.classList.contains('hidden') ) {
+    toggleTableContent();
+  }
+});
